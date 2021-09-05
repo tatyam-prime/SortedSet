9cb272f8c7498d87c1733561da458545f329e5bd
tatyam <tatuhito0125@yahoo.co.jp>
2021-09-05 17:31:49 +0900

    add example

---
+++ example/ABC217-D.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/abc217/submissions/25630663
+
+# paste SortedSet here
+
+import sys
+input = sys.stdin.readline
+
+L, Q = map(int, input().split())
+s = SortedSet([0, L])
+for i in range(Q):
+	c, x = map(int, input().split())
+	if c == 1:
+		s.add(x)
+	else:
+		print(s.gt(x) - s.lt(x))
