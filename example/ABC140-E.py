147a1e59f1ccf23805cf5d7fd93cfa381135dd85
tatyam <tatuhito0125@yahoo.co.jp>
2021-12-31 00:12:30 +0900

    add example for Multiset

---
+++ example/ABC140-E.py
@@ -0,0 +1,27 @@
+# https://atcoder.jp/contests/abc140/submissions/28241859
+
+# paste SortedMultiset here
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+P = list(map(int, input().split()))
+Q = [0] * N
+for i in range(N):
+    Q[P[i] - 1] = i
+
+s = SortedMultiset([-1, -1, N, N])
+ans = 0
+cnt = 0
+for x in reversed(Q):
+    s.add(x)
+    i = s.index(x)
+    v = s[i - 2]
+    w = s[i - 1]
+    y = s[i + 1]
+    z = s[i + 2]
+    cnt += (z - y) * (x - w) + (y - x) * (w - v)
+    ans += cnt
+
+print(ans)
