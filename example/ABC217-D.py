13e86f5b4806b300a1cf7abfc7addbb6d780344e
tatyam <tatuhito0125@yahoo.co.jp>
2022-01-08 22:45:23 +0900

    add example

--- example/ABC217-D.py
+++ example/ABC217-D.py
@@ -8,8 +8,8 @@ input = sys.stdin.readline
 L, Q = map(int, input().split())
 s = SortedSet([0, L])
 for i in range(Q):
-	c, x = map(int, input().split())
-	if c == 1:
-		s.add(x)
-	else:
-		print(s.gt(x) - s.lt(x))
+    c, x = map(int, input().split())
+    if c == 1:
+        s.add(x)
+    else:
+        print(s.gt(x) - s.lt(x))
---
+++ example/ABC234-D.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/abc234/submissions/28401471
+
+# paste SortedSet here
+
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+P = list(map(int, input().split()))
+K -= 1
+s = SortedSet(P[:K])
+
+for i in range(K, N):
+    s.add(P[i])
+    print(s[~K])
