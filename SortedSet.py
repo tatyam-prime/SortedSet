d2259de0298f7b0be4855c81042f3f3b21656413
tatyam <tatuhito0125@yahoo.co.jp>
2022-01-10 03:33:58 +0900

    二分探索しない方が速かった

--- SortedSet.py
+++ SortedSet.py
@@ -1,11 +1,11 @@
 # https://github.com/tatyam-prime/SortedSet/blob/main/SortedSet.py
 import math
 from bisect import bisect_left, bisect_right
-from typing import Generic, Iterable, Iterator, TypeVar, Union
+from typing import Generic, Iterable, Iterator, TypeVar, Union, List
 T = TypeVar('T')
 
 class SortedSet(Generic[T]):
-    BUCKET_RATIO = 50
+    BUCKET_RATIO = 60
     REBUILD_RATIO = 170
 
     def _build(self, a=None) -> None:
@@ -40,21 +40,16 @@ class SortedSet(Generic[T]):
         s = str(list(self))
         return "{" + s[1 : len(s) - 1] + "}"
 
-    def _bucket_index(self, x: T) -> int:
-        "Find the index of the bucket which should contain x. self must not be empty. / O(log N)"
-        ok = 0
-        ng = len(self.a)
-        a = self.a
-        while ng - ok > 1:
-            mid = (ng + ok) >> 1
-            if a[mid][0] <= x: ok = mid
-            else: ng = mid
-        return ok
+    def _find_bucket(self, x: T) -> List[T]:
+        "Find the list which should contain x. self must not be empty."
+        for a in self.a:
+            if x <= a[-1]:
+                return a
+        return a
 
     def __contains__(self, x: T) -> bool:
-        "O(log N)"
         if self.size == 0: return False
-        a = self.a[self._bucket_index(x)]
+        a = self._find_bucket(x)
         i = bisect_left(a, x)
         return i != len(a) and a[i] == x
 
@@ -64,7 +59,7 @@ class SortedSet(Generic[T]):
             self.a = [[x]]
             self.size = 1
             return True
-        a = self.a[self._bucket_index(x)]
+        a = self._find_bucket(x)
         i = bisect_left(a, x)
         if i != len(a) and a[i] == x: return False
         a.insert(i, x)
@@ -76,7 +71,7 @@ class SortedSet(Generic[T]):
     def discard(self, x: T) -> bool:
         "Remove an element and return True if removed. / O(√N)"
         if self.size == 0: return False
-        a = self.a[self._bucket_index(x)]
+        a = self._find_bucket(x)
         i = bisect_left(a, x)
         if i == len(a) or a[i] != x: return False
         a.pop(i)
@@ -85,52 +80,40 @@ class SortedSet(Generic[T]):
         return True
     
     def lt(self, x: T) -> Union[T, None]:
-        "Find the largest element < x, or None if it doesn't exist. / O(log N)"
-        if self.size == 0: return
-        i = self._bucket_index(x)
-        a = self.a
-        if a[i][0] >= x:
-            return a[i - 1][-1] if i else None
-        return a[i][bisect_left(a[i], x) - 1]
+        "Find the largest element < x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] < x:
+                return a[bisect_left(a, x) - 1]
 
     def le(self, x: T) -> Union[T, None]:
-        "Find the largest element <= x, or None if it doesn't exist. / O(log N)"
-        if self.size == 0: return
-        i = self._bucket_index(x)
-        a = self.a
-        if a[i][0] > x:
-            return a[i - 1][-1] if i else None
-        return a[i][bisect_right(a[i], x) - 1]
+        "Find the largest element <= x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] <= x:
+                return a[bisect_right(a, x) - 1]
 
     def gt(self, x: T) -> Union[T, None]:
-        "Find the smallest element > x, or None if it doesn't exist. / O(log N)"
-        if self.size == 0: return
-        i = self._bucket_index(x)
-        a = self.a
-        if a[i][-1] <= x:
-            return a[i + 1][0] if i + 1 < len(self.a) else None
-        return a[i][bisect_right(a[i], x)]
+        "Find the smallest element > x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] > x:
+                return a[bisect_right(a, x)]
 
     def ge(self, x: T) -> Union[T, None]:
-        "Find the smallest element >= x, or None if it doesn't exist. / O(log N)"
-        if self.size == 0: return
-        i = self._bucket_index(x)
-        a = self.a
-        if a[i][-1] < x:
-            return a[i + 1][0] if i + 1 < len(self.a) else None
-        return a[i][bisect_left(a[i], x)]
+        "Find the smallest element >= x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] >= x:
+                return a[bisect_left(a, x)]
     
     def __getitem__(self, x: int) -> T:
-        "Return the x-th element, or IndexError if it doesn't exist. / O(√N) (fast)"
+        "Return the x-th element, or IndexError if it doesn't exist."
         if x < 0: x += self.size
-        if x < 0 or x >= self.size: raise IndexError
+        if x < 0: raise IndexError
         for a in self.a:
             if x < len(a): return a[x]
             x -= len(a)
-        assert False  # unreachable
+        raise IndexError
     
     def index(self, x: T) -> int:
-        "Count the number of elements < x. / O(√N) (fast)"
+        "Count the number of elements < x."
         ans = 0
         for a in self.a:
             if a[-1] >= x:
@@ -139,7 +122,7 @@ class SortedSet(Generic[T]):
         return ans
 
     def index_right(self, x: T) -> int:
-        "Count the number of elements <= x. / O(√N) (fast)"
+        "Count the number of elements <= x."
         ans = 0
         for a in self.a:
             if a[-1] > x:
--- example/ABC217-D.py
+++ example/ABC217-D.py
@@ -13,3 +13,10 @@ for i in range(Q):
         s.add(x)
     else:
         print(s.gt(x) - s.lt(x))
+        # ↓ faster
+        # a = s.a
+        # for i in range(len(a)):
+        #     if a[i][-1] > x:
+        #         j = bisect_left(a[i], x)
+        #         print(a[i][j] - (a[i][j - 1] if j else a[i - 1][-1]))
+        #         break
