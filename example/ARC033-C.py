de1cb6077ffd434880b1d43dd8b4c5520a723512
tatyam <tatuhito0125@yahoo.co.jp>
2023-01-03 16:37:57 +0900

    Create ARC033-C.py

---
+++ example/ARC033-C.py
@@ -0,0 +1,17 @@
+# https://atcoder.jp/contests/arc033/submissions/37710207
+
+# paste SortedMultiset here
+
+import sys
+input = sys.stdin.readline
+
+Q = int(input())
+s = SortedMultiset()
+for _ in range(Q):
+    t, x = map(int, input().split())
+    if t == 1:
+        s.add(x)
+    else:
+        x = s[x - 1]
+        print(x)
+        s.discard(x)
