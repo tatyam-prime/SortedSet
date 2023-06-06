2a4be799c2cc067149aa0ad143d36a286a26d87d
tatyam <tatuhito0125@yahoo.co.jp>
2023-06-06 17:42:16 +0900

    add ABC281-E.py

---
+++ example/ABC281-E.py
@@ -0,0 +1,33 @@
+# https://atcoder.jp/contests/abc294/submissions/42037264
+
+# paste SortedMultiset here
+
+import sys
+input = sys.stdin.readline
+
+N, M, K = map(int, input().split())
+A = list(map(int, input().split()))
+first = sorted(A[:M])
+low = SortedMultiset(first[:K])
+high = SortedMultiset(first[K:])
+ans = sum(first[:K])
+print(ans)
+
+for i in range(M, N):
+    x = A[i]
+    if x < low[-1]:
+        ans -= low[-1]
+        high.add(low.pop(-1))
+        ans += x
+        low.add(x)
+    else:
+        high.add(x)
+    x = A[i - M]
+    if x < high[0]:
+        ans -= x
+        low.discard(x)
+        ans += high[0]
+        low.add(high.pop(0))
+    else:
+        high.discard(x)
+    print(ans)
